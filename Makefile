12e49ed22dff81af615e2f3f2646193ffbf1e000
ps68060 <p.slegg@scubadivers.co.uk>
2017-12-08 15:29:48 +0000

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,5 @@
+[Desktop Entry]
+Type=Link
+Name=Makefile
+Icon=text-x-makefile
+URL=smb://milan/tmp/tmp/Makefile
---
+++ gemasist.c
@@ -0,0 +1,5 @@
+[Desktop Entry]
+Type=Link
+Name=gemasist.c
+Icon=text-x-csrc
+URL=smb://milan/tmp/tmp/gemasist.c
