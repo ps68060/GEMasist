e309c41188c1f37bfbb60c5b8dd9e9382600d1ea
ps68060 <p.slegg@scubadivers.co.uk>
2017-12-16 13:38:06 +0000

    Move function definitions to header.

--- gemasist.c
+++ gemasist.c
@@ -28,22 +28,6 @@
 
 # define pexec(m,f,a,b)   Pexec (m, f, a, b)
 
-void ApTerm( void);
-
-void winfo( WINDOW *win, int index);
-void wclose( WINDOW *win, int index);
-void wCheck( WINDOW *win, int index, int mode, char* appName);
-void wFsel( WINDOW *win, int index);
-
-void makeConfig(char* appName);
-
-void AddTextLabel( void *dial, int parent, char *obj_label);
-void AddCheckBox( void *dial, int parent, ezxml_t object, char *obj_label);
-void AddRadioButtons( void   *dial, int     parent, ezxml_t object, char    *obj_label);
-void AddButton(  void *dial, int parent, char *obj_label);
-
-void RightButton(void);
-
 struct a_buttonFunc
 {
 	int  attach;
--- gemasist.h
+++ gemasist.h
@@ -1,2 +1,19 @@
 #define debug_print(fmt, ...) \
   do { if (DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
+
+
+void ApTerm( void);
+
+void winfo( WINDOW *win, int index);
+void wclose( WINDOW *win, int index);
+void wCheck( WINDOW *win, int index, int mode, char* appName);
+void wFsel( WINDOW *win, int index);
+
+void makeConfig(char* appName);
+
+void AddTextLabel( void *dial, int parent, char *obj_label);
+void AddCheckBox( void *dial, int parent, ezxml_t object, char *obj_label);
+void AddRadioButtons( void   *dial, int     parent, ezxml_t object, char    *obj_label);
+void AddButton(  void *dial, int parent, char *obj_label);
+
+void RightButton(void);
