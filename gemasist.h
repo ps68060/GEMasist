4aec6518ccf721bac801666e2b64cec3ee49172e
ps68060 <pslegg@scubadivers.co.uk>
2018-10-08 13:13:52 +0100

    Fix some warnings from gcc7

--- gemasist.h
+++ gemasist.h
@@ -17,21 +17,41 @@ This file is part of GEMasist.
 */
 
 #define debug_print(fmt, ...) \
-  do { if (DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
+  do { if (DEBUG_ME) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
 
 
-void ApTerm( void);
+struct a_buttonFunc
+{
+  int  attach;
+  int  index;
+  char *param;
+  char *filename;
+};
 
-void winfo( WINDOW *win, int index);
+
+int  ApTerm( void);
+
+void winfo(  WINDOW *win, int index);
 void wclose( WINDOW *win, int index);
 void wCheck( WINDOW *win, int index, int mode, char* appName);
-void wFsel( WINDOW *win, int index);
+int  wFsel(  WINDOW *win, int index, char *filename);
 
 void makeConfig(char* appName);
 
-void AddTextLabel( void *dial, int parent, char *obj_label);
-void AddCheckBox( void *dial, int parent, ezxml_t object, char *obj_label);
-void AddRadioButtons( void   *dial, int     parent, ezxml_t object, char    *obj_label);
-void AddButton(  void *dial, int parent, char *obj_label);
+void AddTextLabel(    void *dial, int parent, char *obj_label);
+
+void AddCheckBox( void         *dial
+                , int          parent
+                , ezxml_t      object
+                , char         *obj_label
+                );
+
+void AddFselButton(  void *dial
+										,int  parent
+										,char *obj_label);
+
+void AddRadioButtons( void *dial, int parent, ezxml_t object, char *obj_label);
+
+void AddButton(       void *dial, int parent, char *obj_label);
 
 void RightButton(void);
